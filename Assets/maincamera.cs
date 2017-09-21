d10484af4cf670153278d7a6a3821a64e6b63a57
ekko <andyt05tw@gmail.com>
2017-09-22 00:35:55 +0800

    origin

---
+++ Assets/maincamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class maincamera : MonoBehaviour
+{
+    public GameObject character;
+    public Vector2 charpos;
+    public bool abc = false;
+    void update()
+       {
+            if (abc)
+            {
+            charpos = character.transform.position;
+            charpos.x--;
+        }
+        }
+    void OnMouseDown()
+    {
+        abc = true;
+    }
+
+
+
+}
+    
+
+    
+
+
+
