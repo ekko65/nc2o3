a17fb1b74566dd35c651ec65a6c3bfdd90d47bee
ekko65 <andyt05tw@gmail.com>
2018-04-11 02:00:50 +0800

    2018/04/11 備份

---
+++ Assets/stage/nahan/clickdownforbl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickdownforbl : MonoBehaviour
+{
+    //指向管理colorBlock得物件，此物件必須要colorblock.cs的程式
+    public GameObject controlBox;
+    private GameObject theTarget;
+
+    public GameObject self;
+    // Use this for initialization
+    void Start()
+    {
+        // theTarget = controlBox.GetComponent<colorblock>().target;
+
+    }
+    void OnMouseUp()
+    {
+        controlBox.GetComponent<colorblock>().target = self;
+        controlBox.GetComponent<colorblock>().jumpAction = true;
+    }
+}
--- Assets/stage/yishuan/shoottothrid.cs
+++ Assets/stage/yishuan/shoottothrid.cs
@@ -1,28 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class shoottothrid : MonoBehaviour {
+public class shoottothrid : MonoBehaviour
+{
     public GameObject charaterSoul;
-    public Transform transform;
+    public Transform charaterTransform;
     public Vector3 toGo;
     public Vector3 move;
     public float speed = 1f;//移動速度
     // Use this for initialization
-    void Awake() {
+    void Awake()
+    {
         // toGo = Vector3(2.15, 12.53, 0);
-        transform = charaterSoul.GetComponent<Transform>();
-        // move = (toGo - transform.position)/50;
+        charaterTransform = charaterSoul.GetComponent<Transform>();
+        // move = (toGo - charaterTransform.position)/50;
     }
 
     void Update()
     {
         //讓使用者每按一次 ↑ 時都移動一次，這只是為了方便看出每次移動的距離
-        if (transform.position.y <12.5)
+        if (charaterTransform.position.y < 12.5)
         {
-            transform.position = toGo;
-            // transform.position = Vector3.Lerp(transform.position, Vector3(2.15, 12.53, 0), speed);
+            charaterTransform.position = toGo;
+            // charaterTransform.position = Vector3.Lerp(charaterTransform.position, Vector3(2.15, 12.53, 0), speed);
             // speed = calculateNewSpeed();
-        } else {
+        }
+        else
+        {
             return;
         }
     }
